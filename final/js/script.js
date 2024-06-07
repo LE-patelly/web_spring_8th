1c564df99de762883c28c5f2fb93719359deca65
LE-patelly <epatel@eastsideprep.org>
2024-06-07 10:51:08 -0700

    Add files via upload

---
+++ final/js/script.js
@@ -0,0 +1,8 @@
+function phoneNumber() {
+   var n = document.getElementById('num');
+   n.innerHTML = '425-625-6874';
+}
+function email() {
+    var l = document.getElementById('em');
+    l.innerHTML = 'elinaisagenius@icloud.com';
+ }
